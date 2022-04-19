f288af9c429400874d5ea7aff40361417c693a0c
Lucas Sacksobar <lucass.p.f.c.lf9@gmail.com>
2022-04-19 20:17:54 -0300

    frase anime

--- comandos/respostas.py
+++ comandos/respostas.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+import requests
+import json
 
 class Resposta(commands.Cog):
     """ Reage às mensagens """
@@ -9,6 +11,16 @@ class Resposta(commands.Cog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
+        if message.content == "opa":
+            msg = "OPA EAE VEI BLZ. G2AAAAA"
+            await message.channel.send(msg)
+        if message.content == "anime":
+            msg = requests.get('https://animechan.vercel.app/api/random').json()
+            frase = msg['quote']
+            frase = f'"{frase[0:-1]}"'
+            anime = ' Anime: ' + msg['anime']
+            personagem = " - " + msg['character']
+            await message.channel.send(frase + personagem + '\n' + anime)
             
     
     @commands.Cog.listener()
