e655c3c2edd5a6fdda505df21d90d65a0418ebb8
Lucas Sacksobar <lucass.p.f.c.lf9@gmail.com>
2022-04-21 17:21:12 -0300

    mensagem às 4 e meia da manhã

--- comandos/respostas.py
+++ comandos/respostas.py
@@ -1,7 +1,7 @@
+from xmlrpc import client
 from discord.ext import commands
 import requests
 import json
-
 class Resposta(commands.Cog):
     """ Reage às mensagens """
     def __init__(self, bot):
@@ -14,20 +14,10 @@ class Resposta(commands.Cog):
         if message.content == "opa":
             msg = "OPA EAE VEI BLZ. G2AAAAA"
             await message.channel.send(msg)
-#        if message.content == "anime":
-#            msg = requests.get('https://animechan.vercel.app/api/random').json()
-#            frase = msg['quote']
-#            frase = f'"{frase[0:-1]}"'
-#            anime = ' Anime: ' + msg['anime']
-#            personagem = " - " + msg['character']
-#            await message.channel.send(frase + personagem + '\n' + anime)
-#            await message.channel.send(f"\ghipy {msg['character']}")
             
-    
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'estou pronto! Estou conectado como {self.bot.user}')
-
 
 def setup(bot):
     bot.add_cog(Resposta(bot))
