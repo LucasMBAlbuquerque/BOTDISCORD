67c4c65ef1b2bdeec8e306d4c1bfcc74acb9b915
Lucas Sacksobar <lucass.p.f.c.lf9@gmail.com>
2022-04-21 21:56:15 -0300

    mensagem diaria sem fazer request da API das frases todo segundo

--- comandos/Dates/msg_diaria.py
+++ comandos/Dates/msg_diaria.py
@@ -20,37 +20,35 @@ class Dates(commands.Cog):
 
     @tasks.loop(seconds=1)
     async def current_time(self, tz = pytz.timezone('America/Cayenne')):
-
-
         now = datetime.datetime.now(tz)
-
         now = now.strftime("%H:%M:%S")
-
-        api_giphy = config("api_giphy")
-        api_instance = giphy_client.DefaultApi()
-        msg = requests.get('https://animechan.vercel.app/api/random').json()
-        frase = msg['quote'][0:-1]
-        anime = ' Anime: ' + msg['anime']
-        personagem1 = f"{msg['anime']} " + msg['character']
-        personagem = " - " + msg['character']
-        try:
-            api_responce = api_instance.gifs_search_get(api_giphy, personagem1, limit=1, rating='r')
-            lst =list(api_responce.data)
-            giff = random.choice(lst)
-
-            #await ctx.send(giff.embed_url)
-            #await ctx.send('\n' + frase + personagem + '\n' + anime)
-
-        except ApiException as e:
-            print('Erro ao usar API do ghipy')
-
         channel = self.bot.get_channel(660320080348839948)
         if now == "04:30:00":
-            translator= Translator(to_lang="pt")
-            translation = translator.translate(frase)
+            api_giphy = config("api_giphy")
+            api_instance = giphy_client.DefaultApi()
+            msg = requests.get('https://animechan.vercel.app/api/random').json()
+            frase = msg['quote'][0:-1]
+            anime = ' Anime: ' + msg['anime']
+            personagem1 = f"{msg['anime']} " + msg['character']
+            personagem = " - " + msg['character']
+            try:
+                api_responce = api_instance.gifs_search_get(api_giphy, personagem1, limit=1, rating='r')
+                lst =list(api_responce.data)
+                giff = random.choice(lst)
+
+                #await ctx.send(giff.embed_url)
+                #await ctx.send('\n' + frase + personagem + '\n' + anime)
+
+            except ApiException as e:
+                print('Erro ao usar API do ghipy')
+                translator= Translator(to_lang="pt")
+                translation = translator.translate(frase)
+
             await channel.send("Bom dia :sunglasses:")
             await channel.send(giff.embed_url)
             await channel.send('\n' + f'"{translation}"' + personagem + '\n' + anime)
+        else:
+            pass
 
 
 def setup(bot):
