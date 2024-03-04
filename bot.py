e9742ef7dc16eccd6f32acd750e985c1dab1bdfd
Lucas Sacksobar <lucass.p.f.c.lf9@gmail.com>
2024-03-04 12:40:30 -0300

    Atualizando Arquivos

--- bot.py
+++ bot.py
@@ -3,21 +3,8 @@ import nest_asyncio
 from decouple import config
 import os
 nest_asyncio.apply()
-#
+
 bot = commands.Bot('!')
-#
-#bot.load_extension("comandos.coinmarketcap.cryptocmk")
-#bot.load_extension("comandos.Imagens.procurado")
-#bot.load_extension("comandos.Imagens.nofap")
-#bot.load_extension("comandos.Imagens.gremio")
-#bot.load_extension("comandos.Imagens.professora")
-#bot.load_extension("comandos.Imagens.npc")
-#bot.load_extension("comandos.Imagens.odio")
-##bot.load_extension("comandos.Animes.gif_anime")
-#bot.load_extension("comandos.Imagens.memerandom")
-#bot.load_extension("comandos.MensagemDiaria.msg_diaria")
-#bot.load_extension("comandos.fazedor_de_pdf.pdf_maker")
-#bot.load_extension("comandos.FrasesAnime.FraseAnime")
 
 def carregar_cogs(bot):
     bot.load_extension("comandos.respostas")
